7167969c0ee082e13bd222afa48b7f19ee68128e
Geovana Carreira <you@example.com>
2023-05-16 18:36:48 -0300

    primeiro commit

---
+++ Aula1.js
@@ -0,0 +1,21 @@
+
+const gasPrice = 5.64;
+const kmPorLitro = 12;
+const kmViagem = 612;
+
+const litrosConsumidos = kmViagem / kmPorLitro;
+const valorGasto = litrosConsumidos * gasPrice;
+
+console.log(valorGasto.toFixed(2));
+
+
+
+
+
+
+
+
+
+
+
+
